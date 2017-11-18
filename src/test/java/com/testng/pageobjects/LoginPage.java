26a76c85ba883806edfdfe091bae8d2c6e3c29a7
rcrao <ramthopucherla@gmail.com>
2017-11-18 10:26:20 +0530

    removed the commented code

--- src/test/java/com/testng/pageobjects/LoginPage.java
+++ src/test/java/com/testng/pageobjects/LoginPage.java
@@ -19,10 +19,6 @@ public class LoginPage {
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
 	}
-
-	/*private By emailId = By.id("email");
-	private By password = By.name("pass");
-	private By loginButton = By.xpath(".//*[@id='u_0_5']");*/
 	
 	@FindBy(id="email") 
 	@CacheLookup
@@ -38,11 +34,6 @@ public class LoginPage {
 	 
 	
 	public void loginFB(String email, String pass) {
-
-		/*driver.findElement(emailId).sendKeys(email);
-		driver.findElement(password).sendKeys(pass);
-		driver.findElement(loginButton).submit();*/
-		
 		emailId.sendKeys(email);
 		password.sendKeys(pass);
 		loginButton.submit();
